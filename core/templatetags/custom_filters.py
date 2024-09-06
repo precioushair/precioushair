ec51dd7c1dfe70c9af8ba38e71ed72ccd6be7133
Chijioke Divine <152617348+Tekdiverse@users.noreply.github.com>
2024-09-06 05:18:51 +0100

    ...

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -6,3 +6,6 @@ register = template.Library()
 def get_quantity(cart, product):
     return cart.get_quantity_of_product(product)
 
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    return field.as_widget(attrs={"class": css_class})
